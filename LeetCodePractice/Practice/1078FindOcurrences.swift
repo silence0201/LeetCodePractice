e2061f84aede4fffca2632b8f40d8981a2a0f443
Silence <374619540@qq.com>
2021-12-26 16:10:00 +0800

    1078. Bigram 分词

---
+++ LeetCodePractice/Practice/1078FindOcurrences.swift
@@ -0,0 +1,38 @@
+/* 1078. Bigram 分词
+* 给出第一个词 first 和第二个词 second，考虑在某些文本 text 中可能以 "first second third" 形式出现的情况，其中 second 紧随 first 出现，third 紧随 second 出现。
+*
+* 对于每种这样的情况，将第三个词 "third" 添加到答案中，并返回答案。
+*
+* 示例 1：
+*
+* 输入：text = "alice is a good girl she is a good student", first = "a", second = "good"
+* 输出：["girl","student"]
+* 示例 2：
+*
+* 输入：text = "we will we will rock you", first = "we", second = "will"
+* 输出：["we","rock"]
+*
+* 来源：力扣（LeetCode）
+* 链接：https://leetcode-cn.com/problems/occurrences-after-bigram
+* 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+import XCTest
+
+class Solution {
+    func findOcurrences(_ text: String, _ first: String, _ second: String) -> [String] {
+        let subString = text.components(separatedBy: " ")
+        
+        if (subString.count < 2) {
+            return []
+        }
+        
+        var ans = [String]()
+        for i in 2..<subString.count {
+            if (subString[i-1] == second && subString[i - 2] == first) {
+                ans.append(subString[i])
+            }
+        }
+        return ans
+    }
+}
